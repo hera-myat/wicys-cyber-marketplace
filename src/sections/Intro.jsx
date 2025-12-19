49c44db6b302e006aaf2fb1c1ed21893a08d3b7b
acama5 <acama@uic.edu>
2025-12-18 20:45:21 -0600

    modified Intro by updating the logo and navbar

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -1,8 +1,11 @@
+import logo from "../graphics/wicys-sample-logo.png"
 export default function Intro() {
   return (
     <section className="intro">
       <h1>WICYS CYBER MARKETPLACE</h1>
       <p>EXPLORE WICYS FUNDRASING EVENTS & EXCLUSIVE MERCH & MORE</p>
+      <img src={logo}
+      className = "introLogo" />
     </section>
   )
 }
