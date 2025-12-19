c8fa84ac0b59df658665db6c74266e8755b8efa1
hera-myat <kmyat2@uic.edu>
2025-12-18 20:12:13 -0600

    updated intro section

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -1,8 +1,16 @@
+import wicysLogo from '../graphics/wicys-sample-logo.png'
+
 export default function Intro() {
   return (
     <section className="intro">
       <h1>WICYS CYBER MARKETPLACE</h1>
-      <p>EXPLORE WICYS FUNDRASING EVENTS & EXCLUSIVE MERCH & MORE</p>
+      <p>EXPLORE WICYS FUNDRAISING EVENTS & EXCLUSIVE MERCH & MORE</p>
+
+      <img 
+        src={wicysLogo} 
+        alt="WiCyS Logo" 
+        className="intro-logo"
+      />
     </section>
   )
 }
