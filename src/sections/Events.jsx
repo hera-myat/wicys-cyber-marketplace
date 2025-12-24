845f4d0a29c555e84e8a7558e669f8c11484a04b
PinkCheese GreenGoes <pinkcheesegreengoes@Laptop-de-PinkCheese.local>
2025-12-23 20:03:07 -0600

    updating section 2

--- src/sections/Events.jsx
+++ src/sections/Events.jsx
@@ -1,11 +1,22 @@
 import Section from '../components/Section'
+import coldstone from '../graphics/coldstone-event.png'
+import Rasoi from '../graphics/diwali-event.png'
+import Gathers from '../graphics/gathers-event.png'
+
 
 export default function Events() {
   return (
-    <Section id="events" title="Fundrasing Events">
-      <div className="card">
-        <h3>Event 1</h3>
-        <p>.....</p>
+    <Section id="events" title=" WICYS FUNDRAISING EVENTS">
+      <div className="Scroll">
+        <div className="Photos">
+          <img src={coldstone} alt="Coldstone"/>
+        </div>
+        <div className="Photos">
+          <img src={Rasoi} alt="Rasoi"/>
+        </div>
+        <div className="Photos">
+          <img src={Gathers} alt="Gathers"/>
+        </div>
       </div>
     </Section>
   )
