4675e985e0b01f0a3bae7fe62bfe9e140a55ba34
hera-myat <kmyat2@uic.edu>
2025-12-23 21:20:54 -0600

    updating style

--- src/sections/Events.jsx
+++ src/sections/Events.jsx
@@ -1,11 +1,24 @@
 import Section from '../components/Section'
 
+import coldstone from '../graphics/coldstone-event.png'
+import dawali from '../graphics/diwali-event.png'
+import gathers from '../graphics/gathers-event.png'
+
 export default function Events() {
   return (
-    <Section id="events" title="Fundrasing Events">
-      <div className="card">
-        <h3>Event 1</h3>
-        <p>.....</p>
+    <Section id="events" title="WiCyS Fundraising Events">
+      <div className="events-scroll">
+        <div className="event-card">
+          <img src={coldstone} alt="Coldstone Fundraiser" />
+        </div>
+
+        <div className="event-card">
+          <img src={dawali} alt="Dawali Fundraiser" />
+        </div>
+
+        <div className="event-card">
+          <img src={gathers} alt="Gathers Fundraiser" />
+        </div>
       </div>
     </Section>
   )
