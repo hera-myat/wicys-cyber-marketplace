9e307d4444d7f39d7aff6f2cbf86006a492d7f76
acama5 <acama@uic.edu>
2026-01-06 16:44:25 -0600

    Worked on sections three and four

--- src/sections/Donate.jsx
+++ src/sections/Donate.jsx
@@ -3,7 +3,19 @@ import Section from '../components/Section'
 export default function Donate() {
   return (
     <Section id="donate" title="Support Our Mission">
-      <button className="cta-button">DONATE</button>
+      <div className='donation'>        
+      
+        <h1>AS A NEW ORGANIZATION, YOUR SUPPORT IS MUCH NEEDED. 
+            IF YOU WANT TO SUPPORT WICYS, PLEASE FILL OUT THE FORM BELOW.
+            YOU WILL RECEIVE MEMBER BENIFITS AND EXCLUSIVE MERCH PACK UPON
+            YOUR DONATION.
+        </h1>
+        <h1>WE APPRECIATE YOUR SUPPORT</h1>
+        <h1>▪WICYS BOARD</h1>
+
+        <button className="donateButton">DONATE</button>
+
+      </div>
     </Section>
   )
 }
--- src/sections/Marketplace.jsx
+++ src/sections/Marketplace.jsx
@@ -1,11 +1,47 @@
 import Section from '../components/Section'
+import merchOne from '../graphics/wicys-merch-1.png'
+import merchTwo from '../graphics/wicys-merch-2.png'
+import merchThree from '../graphics/wicys-merch-3.png'
 
 export default function Marketplace() {
   return (
     <Section id="marketplace" title="WiCyS Marketplace">
-      <div className="card">
-        <h3>Merch 1</h3>
-        <p>.....</p>
+      <div className="Marketplace">
+
+        <div className='scrollTitle'>
+           <h1>BECOME A WICYS MEMBER TODAY TO RECIVE MERCH
+            ▪ BECOME A WICYS MEMBER TODAY TO RECIVE MERCH
+            ▪ BECOME A WICYS MEMBER TODAY TO RECIVE MERCH
+            ▪ BECOME A WICYS MEMBER TODAY TO RECIVE MERCH
+            ▪ BECOME A WICYS MEMBER TODAY TO RECIVE MERCH
+           </h1>
+        </div>
+
+        <div className='firstRow'>
+          <div className='merch'>
+            <img src={merchOne} />
+            <h2> WICYS SWEATER $20 </h2>
+          </div>
+
+          <div className='merch'>
+            <img src={merchTwo} />
+            <h2> WICYS TOTE $10 </h2>
+          </div>
+        </div>
+
+        <div className='secondRow'>
+          <div className='merch'>
+            <img src={merchThree} />
+            <h2> WICYS SWEATER $20 </h2>
+          </div>
+
+          <div className='merch'>
+            <h1> UNLOCK EXCLUSIVE REWARDS BY JOINING WICYS TODAY </h1>
+            <h2> WICYS TOTE $10 </h2>
+          </div>
+        </div>
+        
+
       </div>
     </Section>
   )
