9cafeb26b4f6b2ad9354d0de4d68f21d9bc644e3
hera-myat <kmyat2@uic.edu>
2026-01-06 22:05:23 -0600

    updating events

--- src/sections/Events.jsx
+++ src/sections/Events.jsx
@@ -6,20 +6,37 @@ import gathers from '../graphics/gathers-event.png'
 
 export default function Events() {
   return (
-    <Section id="events" title="WiCyS Fundraising Events">
+    <Section id="events" title="WICYS FUNDRAISING EVENTS">
+
       <div className="events-scroll">
         <div className="event-card">
           <img src={coldstone} alt="Coldstone Fundraiser" />
+          <p className="event-date">JAN 3RD</p>
         </div>
 
         <div className="event-card">
-          <img src={dawali} alt="Dawali Fundraiser" />
+          <img src={dawali} alt="Diwali Fundraiser" />
+          <p className="event-date">FRD 4TH</p>
         </div>
 
         <div className="event-card">
           <img src={gathers} alt="Gathers Fundraiser" />
+          <p className="event-date">COMING SOON</p>
         </div>
       </div>
+
+      {/* SCROLL INDICATOR */}
+      <div className="scroll-dots">
+        <span className="dot active"></span>
+        <span className="dot"></span>
+        <span className="dot"></span>
+      </div>
+
+      {/* SUPPORT MESSAGE */}
+      <p className="support-text">
+        SUPPORT WICYS FUNDRAISERS BY MENTIONING WICYS AT CHECKOUT! NO EXTRA PAY
+      </p>
+
     </Section>
   )
 }
