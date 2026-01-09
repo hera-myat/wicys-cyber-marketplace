0b73c09268a3dece13835c55b9a9c49079c6fd59
acama5 <acama@uic.edu>
2026-01-08 19:09:53 -0600

    modified the button

--- src/sections/Donate.jsx
+++ src/sections/Donate.jsx
@@ -13,7 +13,12 @@ export default function Donate() {
         <h1>WE APPRECIATE YOUR SUPPORT</h1>
         <h1>▪WICYS BOARD</h1>
 
-        <button className="donateButton">DONATE</button>
+        
+        <div>
+          <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+            <button className="donateButton">DONATE</button>
+          </a>
+        </div>
 
       </div>
     </Section>
